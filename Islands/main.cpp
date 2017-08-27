d5b12510c8810bf3d5ecd339abddca17c3516920
Szymon Bednorz <dsonyyy@gmail.com>
2017-08-27 21:05:58 +0200

    Configure license headers.

--- Islands/main.cpp
+++ Islands/main.cpp
@@ -1,5 +1,15 @@
+/*
+ *	Program:	Islands
+ *	License:	GNU GPL 2.0
+ *	Author:		dsonyy
+ *	GitHub:		https://github.com/dsonyy/Islands
+ */
+
+
 #include <iostream>
 #include <vector>
+
+#include <SFML\Graphics.hpp>
 
 int main()
 {
