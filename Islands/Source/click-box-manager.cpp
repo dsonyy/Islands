828d89ba6b3e330abad764aa66bd99166cbb9bd1
Szymon Bednorz <dsonyyy@gmail.com>
2017-09-11 22:21:26 +0200

    ClickBoxManager update.

--- Islands/Source/click-box-manager.cpp
+++ Islands/Source/click-box-manager.cpp
@@ -4,42 +4,64 @@
 
 #include "click-box-manager.h"
 
-std::vector<sf::Drawable> ClickBoxManager::Hover(const sf::Vector2i & pos)
+std::vector<ClickBox> ClickBoxManager::Hover(const sf::Vector2i & pos)
 {
-	for each (auto & d in drawables_)
-	{
+	std::vector<ClickBox> clicked;
 
+	for each (auto & d in click_boxes_)
+	{
+		//if (IsPointInsideRect(pos.x, pos.y,
+		//	d.getPosition().x, d.getPosition().y,
+		//	d.getPosition().x + d., d.getPosition().y))
+	}
+	return clicked;
+}
+
+std::vector<ClickBox> ClickBoxManager::Click(const sf::Vector2i & pos)
+{
+	std::vector<ClickBox> clicked;
+
+	return clicked;
+}
+
+std::vector<ClickBox> ClickBoxManager::Release(const sf::Vector2i & pos)
+{
+	std::vector<ClickBox> clicked;
+
+
+	return clicked;
+}
+
+std::vector<ClickBox> ClickBoxManager::Hover(size_t pos_x, size_t pos_y)
+{
+	std::vector<ClickBox> clicked;
+
+
+	return clicked;
+}
+
+std::vector<ClickBox> ClickBoxManager::Click(size_t pos_x, size_t pos_y)
+{
+	std::vector<ClickBox> clicked;
+
+	return clicked;
+}
+
+std::vector<ClickBox> ClickBoxManager::Release(size_t pos_x, size_t pos_y)
+{
+	std::vector<ClickBox> clicked;
+
+	return clicked;
+}
+
+bool ClickBoxManager::IsPointInsideRect(size_t px, size_t py,
+	size_t x0, size_t y0, size_t x1, size_t y1) const
+{
+	if (px >= x0 && px <= x1 &&
+		py >= y0 && py <= y1)
+	{
+		return true;
 	}
 
-	return std::vector<sf::Drawable>();
-}
-
-std::vector<sf::Drawable> ClickBoxManager::Click(const sf::Vector2i & pos)
-{
-
-	return std::vector<sf::Drawable>();
-}
-
-std::vector<sf::Drawable> ClickBoxManager::Release(const sf::Vector2i & pos)
-{
-
-	return std::vector<sf::Drawable>();
-}
-
-std::vector<sf::Drawable> ClickBoxManager::Hover(size_t pos_x, size_t pos_y)
-{
-
-	return std::vector<sf::Drawable>();
-}
-
-std::vector<sf::Drawable> ClickBoxManager::Click(size_t pos_x, size_t pos_y)
-{
-
-	return std::vector<sf::Drawable>();
-}
-
-std::vector<sf::Drawable> ClickBoxManager::Release(size_t pos_x, size_t pos_y)
-{
-
-	return std::vector<sf::Drawable>();
+	return false;
 }
--- Islands/Source/click-box-manager.h
+++ Islands/Source/click-box-manager.h
@@ -4,22 +4,27 @@
 
 #include <SFML/Graphics.hpp>
 
-
+typedef sf::Rect<sf::Vector2f> ClickBox;
 
 class ClickBoxManager
 {
 public:
+
+
 	ClickBoxManager() {}
 	~ClickBoxManager() {}
 
-	std::vector<sf::Drawable> Hover(const sf::Vector2i & pos);
-	std::vector<sf::Drawable> Click(const sf::Vector2i & pos);
-	std::vector<sf::Drawable> Release(const sf::Vector2i & pos);
+	std::vector<ClickBox> Hover(const sf::Vector2i & pos);
+	std::vector<ClickBox> Click(const sf::Vector2i & pos);
+	std::vector<ClickBox> Release(const sf::Vector2i & pos);
 	
-	std::vector<sf::Drawable> Hover(size_t pos_x, size_t pos_y);
-	std::vector<sf::Drawable> Click(size_t pos_x, size_t pos_y);
-	std::vector<sf::Drawable> Release(size_t pos_x, size_t pos_y);
+	std::vector<ClickBox> Hover(size_t pos_x, size_t pos_y);
+	std::vector<ClickBox> Click(size_t pos_x, size_t pos_y);
+	std::vector<ClickBox> Release(size_t pos_x, size_t pos_y);
 
 private:
-	std::vector<sf::Drawable> drawables_;
+	bool IsPointInsideRect(size_t px, size_t py, 
+		size_t x0, size_t y0, size_t x1, size_t y1) const;
+
+	std::vector<ClickBox> click_boxes_;
 };
